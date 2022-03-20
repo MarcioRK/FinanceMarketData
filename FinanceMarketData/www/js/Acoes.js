40096aba318014252cc9caa8a6c9ff854b728e79
William Massao <william.massao.marubayashi@gmail.com>
2022-03-20 19:29:21 -0300

    Inicio da Implementacao na Tela de Acoes

--- FinanceMarketData/www/js/Acoes.js
+++ FinanceMarketData/www/js/Acoes.js
@@ -1,18 +1,20 @@
-function getStock(stockId)
+function getStock()
 {
-    var url = 'https://cors-anywhere.herokuapp.com/https://finnhub.io/api/v1/quote?symbol=' + stockId;
+    codigoAcao = document.getElementById('txtAcao').value;
+    var lblCodigo = document.getElementById('lblCodigoAcao')
+    var lblValorAcao = document.getElementById('lblValorAcao')
+
+    var url = 'https://cors-anywhere.herokuapp.com/https://finnhub.io/api/v1/quote?symbol=' + codigoAcao;
     var request = new XMLHttpRequest();
     var keyValue = 'c8q02k2ad3icps1k1fjg';
     request.open('GET', url);
     request.setRequestHeader('X-Finnhub-Token', keyValue);
     request.send();
     request.onload = function(){
-        var responseJson = JSON.parse(request.response)
-        console.log(responseJson.c)
+        var responseJson = JSON.parse(request.response);
+        console.log(responseJson.c);
+        lblCodigo.innerHTML = String(codigoAcao)
+        lblValorAcao.innerHTML = "R$ " + responseJson.c;
     }
 }
 
-window.onload = function()
-{
-    getStock('AAPL')
-}

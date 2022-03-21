5609d580389192e914d48fceb1a2d7ccefdddbf6
William Massao <william.massao.marubayashi@gmail.com>
2022-03-21 00:45:27 -0300

    Finalizacao Implementacao da API de Covid

---
+++ FinanceMarketData/www/js/Covid.js
@@ -0,0 +1,68 @@
+function getProvinceCountryList()
+{
+    select = document.getElementById('cboEstadosPaises');
+    select.disabled = true;
+
+    var url = 'https://coronavirus.m.pipedream.net/';
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.send();
+    request.onload = function(){
+        if (request.status === 200){
+            select.disabled = false;
+            var responseJson = JSON.parse(request.response);
+            for (var i = 0; i<=responseJson.rawData.length; i++){
+                try {
+                    if (responseJson.rawData[i].Combined_Key != undefined){
+                        var opt = document.createElement('option');
+                        opt.value = responseJson.rawData[i].Combined_Key;
+                        opt.innerHTML = responseJson.rawData[i].Combined_Key;
+                        select.appendChild(opt);
+                    }      
+                } catch (error) {
+                    console.log("Erro")
+                }
+            }
+        }
+    }
+}
+
+function getCovidDataBasedOnCountry(){
+        estadoPaisSelecionado= document.getElementById("cboEstadosPaises").value;
+        txtCasosTotais = document.getElementById("txtCasosTotais");
+        txtTotalObitos = document.getElementById("txtTotalObito");
+        txtTaxaIncidencia = document.getElementById("txtTaxaIncidencia");
+        txtTaxaobito = document.getElementById("txtTaxaObito");
+
+        var url = 'https://coronavirus.m.pipedream.net/';
+        var request = new XMLHttpRequest();
+        request.open('GET', url);
+        request.send();
+        txtCasosTotais.innerText = "Loading..."
+        txtTotalObitos.innerText = "Loading..."
+        txtTaxaIncidencia.innerText = "Loading..."
+        txtTaxaobito.innerText = "Loading..."
+        request.onload = function(){
+            if (request.status === 200){
+                var responseJson = JSON.parse(request.response);
+                for (var i = 0; i<=responseJson.rawData.length; i++){
+                    try {
+                        console.log(estadoPaisSelecionado)
+                        if (responseJson.rawData[i].Combined_Key == estadoPaisSelecionado){
+                            txtCasosTotais.innerText = responseJson.rawData[i].Confirmed;
+                            txtTotalObitos.innerText = responseJson.rawData[i].Deaths;
+                            txtTaxaIncidencia.innerText = responseJson.rawData[i].Incident_Rate;
+                            txtTaxaobito.innerText = responseJson.rawData[i].Case_Fatality_Ratio;
+                            break;
+                        }      
+                    } catch (error) {
+                    }
+                }
+            }
+        }
+
+}
+
+window.onload = function() {
+    getProvinceCountryList();
+  };
